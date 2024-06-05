30ae88130d5a3d337a3e82e19968bcfd888a1c41
Roberto Núñez <robertonc1599@gmail.com>
2024-06-05 18:04:27 -0500

    Initial Commit - Folders Structure

---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import {AirplaneIcon} from '../icons/Icons';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <AirplaneIcon />
+    </View>
+  );
+};
---
+++ src/presentation/screens/icons/Icons.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export const AirplaneIcon = () => {
+  return (
+    <View>
+      <Text>AirplaneIcon</Text>
+    </View>
+  );
+};
